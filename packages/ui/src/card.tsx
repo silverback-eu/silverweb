9db60fc93bb9c9015161f27d9bca932460d9f4a5
dontfred <mail@frederikwgrimm.de>
2023-11-20 11:15:40 +0100

    fix: :ambulance: Configered Storybook and Next
    Added to storybook styling from the ui lib and updated next bzw added a new next repo

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -10,21 +10,23 @@ export function Card({
   href: string;
 }): JSX.Element {
   return (
-    <a
-      className="ui-group ui-rounded-lg ui-border ui-border-transparent ui-px-5 ui-py-4 ui-transition-colors hover:ui-border-neutral-700 hover:ui-bg-neutral-800/30"
-      href={`${href}?utm_source=create-turbo&utm_medium=with-tailwind&utm_campaign=create-turbo"`}
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      <h2 className="ui-mb-3 ui-text-2xl ui-font-semibold">
-        {title}{" "}
-        <span className="ui-inline-block ui-transition-transform group-hover:ui-translate-x-1 motion-reduce:ui-transform-none">
-          -&gt;
-        </span>
-      </h2>
-      <p className="ui-m-0 ui-max-w-[30ch] ui-text-sm ui-opacity-50">
-        {children}
-      </p>
-    </a>
+    <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <a className="border border-stone-500 rounded-md" href={href}>
+        <h3>{title}</h3>
+        <p>{children}</p>
+      </a>
+      <a className="border border-stone-500 rounded-md" href={href}>
+        <h3>{title}</h3>
+        <p>{children}</p>
+      </a>
+      <a className="border border-stone-500 rounded-md" href={href}>
+        <h3>{title}</h3>
+        <p>{children}</p>
+      </a>
+      <a className="border border-stone-500 rounded-md" href={href}>
+        <h3>{title}</h3>
+        <p>{children}</p>
+      </a>
+    </div>
   );
 }
