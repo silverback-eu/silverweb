89b07525f806456b4694c0c774286fe9cb6fc900
dontfred <mail@frederikwgrimm.de>
2023-12-15 09:42:44 +0100

    feat: :lipstick: Changed the fav icon
    Chnaged Fav Icon and other meta icons/ images like apple touch, safari and mstile

--- apps/web/config/meta.ts
+++ apps/web/config/meta.ts
@@ -22,7 +22,19 @@ export const meta: Metadata = {
     },
   },
   icons: {
-    icon: ["/icon/favicon.ico?v=4"],
+    icon: [
+      "/icon/favicon.ico?v=4",
+      {
+        url: "/icon/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/icon/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
     other: [
       {
         rel: "apple-touch-icon",
