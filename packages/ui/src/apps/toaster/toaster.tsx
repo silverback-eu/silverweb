f2f65424a95151b484c97cbab86746d2052a4237
dontfred <mail@frederikwgrimm.de>
2024-01-09 10:08:52 +0000

    fix: :lipstick: Update Toaster to shadcn 0.6 Dec
    Updated toaster. Mainly styling

--- packages/ui/src/apps/toaster/toaster.tsx
+++ packages/ui/src/apps/toaster/toaster.tsx
@@ -5,24 +5,32 @@ import type { ToasterProps } from "./types";
 export function Toaster(props: ToasterProps): JSX.Element {
   return (
     <PToaster
+      className="toaster group"
       closeButton
       duration={500000}
       loadingIcon={
-        <Loader2 className="ui-mr-2 ui-h-5 ui-w-5 ui-animate-spin" />
+        <Loader2 className="ui-mr-2 ui-h-5 ui-w-5 ui-animate-spin " />
       }
       toastOptions={{
-        style: {
-          borderRadius: "0.75rem",
-          border: "1px solid hsl(var(--border))",
-          backgroundColor: "hsl(var(--card))",
-          color: "hsl(var(--card-foreground))",
-          padding: "1.5rem",
-          boxShadow:
-            "var(--tw-ring-offset-shadow,0 0 #0000),var(--tw-ring-shadow,0 0 #0000), 0 10px 15px -3px rgba(0,0,0,.1),0 4px 6px -4px rgba(0,0,0,.1)",
-          fontFamily: "var(--graphik-webfont), sans-serif",
+        classNames: {
+          closeButton:
+            "!ui-right-[0.75rem] !ui-left-auto !ui-translate-y-[25%] !ui-translate-x-[35%] !ui-bg-transparent !ui-border-none !ui-text-card-foreground [&>_svg]:ui-w-[15px] [&>_svg]:ui-h-[15px] [&>_svg]:ui-opacity-50 [&>_svg]:ui-transition-opacity [&>_svg]:ui-stroke-2 [&_>svg]:hover:ui-opacity-100",
+          toast:
+            "ui-w-full ui-graphik ui-rounded-xl ui-border ui-bg-card ui-text-card-foreground ui-shadow ui-flex ui-p-3 ui-gap-1 [&>div[data-icon]]:ui-w-[20px] [&>div[data-icon]]:ui-h-[20px] [&>div>.loader]:!ui-p-0 [&>div>.loader]:!ui-border-none [&[data-type='loading']>*]:ui-opacity-50 data-[type='loading']:ui-select-none data-[type='loading']:ui-pointer-events-none",
+          description: "!ui-text-sm ui-opacity-90",
+          title: "!ui-text-sm ui-font-bold",
+          actionButton:
+            "group-[.toast]:ui-bg-primary group-[.toast]:ui-text-primary-foreground",
+          cancelButton:
+            "ui-right-[0.75rem] ui-left-[0.75rem] ui-translate-y-[25%] ui-translate-x-[35%] ui-bg-transparent",
+          error: "ui-text-red-500",
+          info: "ui-text-blue-500",
+          success: "ui-text-green-500",
+          warning: "ui-text-yellow-500",
         },
         unstyled: true,
       }}
+      visibleToasts={9}
       {...props}
     />
   );
